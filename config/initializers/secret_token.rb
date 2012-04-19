26220f3de59e5b38e0e701e781c2f8c909f329ee
Rod Labadie <rodhmir@gmail.com>
2012-04-19 00:57:20 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixtapeapp::Application.config.secret_token = '04d758f4e0e4b0b33409970981403ddd740ef71730b38a64b2614f135343960818b14c15923cadd18f6ebe954e2dfa54936fd4a9abfc4805ee06a3dd777f77aa'
